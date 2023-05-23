c1ebd69824bf4a6638cc170e0b68a75e7a4b2906
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2023-05-23 16:55:13 +0400

    Autoplay gtts audio response

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import streamlit as st
+from gtts import gTTS
+from mutagen.mp3 import MP3 
+import base64 
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 import os
+import time 
 import openai
 import whisper
 import streamlit as st 
@@ -15,11 +18,27 @@ from langchain.schema import (
     HumanMessage,
     AIMessage
 )
-whisper_model = whisper.load_model("small") 
+whisper_model = whisper.load_model("medium") 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 st.set_page_config(page_title="arGPT - An Arabic ChatGPT Streamlit Conversational Bot")
 #Title
 st.title(':green[ar]GPT - An :green[Arabic] ChatGPT Streamlit Conversational Bot')
+def autoplay_audio(file_path, lenn): 
+    with open(file_path,"rb") as f: 
+        data = f.read() 
+        b64 = base64.b64encode(data).decode() 
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        sound = st.empty() 
+        sound.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+        time.sleep(int(lenn)+3)
+        sound.empty() 
 
 def audio_to_text(audio): 
     text_response = whisper_model.transcribe(audio.name,language='ar')['text'] 
@@ -34,11 +53,16 @@ if len(audio_bytes > 0):
     wav_file = open("audio.mp3", "wb")
     wav_file.write(audio_bytes.tobytes())
     user_input = whisper_model.transcribe(wav_file.name,language='ar')['text'] 
-    print(user_input)
 if user_input: 
     st.session_state.messages.append(HumanMessage(content=user_input)) 
     with st.spinner("Processing..."): 
-        response = chat(st.session_state.messages) 
+        response = chat(st.session_state.messages)
+        print(response.content) 
+        #converting AI output to speech mp3 file 
+        tts = gTTS(response.content,lang='ar')  
+        tts.save('response.mp3')
+        aud = MP3('response.mp3')
+        autoplay_audio('response.mp3',aud.info.length)
     st.session_state.messages.append(AIMessage(content=response.content)) 
 messages = st.session_state.get('messages',[]) 
 for i , msg in enumerate(messages[1:]): 
